3f4f58d04d87dd18c14b3213c60170a855b4a3ef
I K <rethen@homelab.com>
2026-03-12 20:02:59 +0000

    Add password validation

--- src/validation.js
+++ src/validation.js
@@ -8,3 +8,10 @@ function sanitizeInput(input) {
 }
 
 module.exports = { validateEmail };
+
+module.exports = { validateEmail };
+
+function validatePassword(password) {
+    return password.length >= 8;
+}
+
