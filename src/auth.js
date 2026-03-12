bb747bc4846dbc54fef5780ad80d2666acb7cd46
I K <rethen@homelab.com>
2026-03-12 20:01:16 +0000

    Implement login function

--- src/auth.js
+++ src/auth.js
@@ -4,3 +4,15 @@ function login(username, password) {
 }
 
 module.exports = { login };
+function checkCredentials(username, password) {  // опечатка в названии
+    return username && password;
+}
+
+console.log('Debug: auth module loaded');  // отладочный код
+
+function login(username, password) {
+    if (checkCredentals(username, password)) {
+        return { success: true, token: 'abc123' };
+    }
+    return { success: false };
+}
