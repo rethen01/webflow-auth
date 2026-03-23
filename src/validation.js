8ec2c26aaeb15320b9b570987ccc1049688f2289
I K <rethen@homelab.com>
2026-03-23 14:43:07 +0000

    Critical security patch: use HTTPS and add input sanitization

--- src/validation.js
+++ src/validation.js
@@ -3,4 +3,8 @@ function validateEmail(email) {
     return email.includes('@');
 }
 
+function sanitizeInput(input) {
+    return input.replace(/[<>]/g, '');  // защита от XSS
+}
+
 module.exports = { validateEmail };
